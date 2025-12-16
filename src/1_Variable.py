c70e0a187ae0a34899e25be9a54eb5a3f67b6b06
ilankumaran <kumaranilan@yahoo.com>
2025-12-16 21:14:38 +0000

    Fist Commit

--- src/1_Variable.py
+++ src/1_Variable.py
@@ -2,5 +2,6 @@ name = "Ilankumaran Ilangovan"
 age = 36
 price = 99.99
 isMale = True
+isFemale = False
 
-print(f" name : {name} , age {age} , price {price} and {isMale}")
+print(f" name : {name} , age {age} , price {price} , isMale {isMale} and {isFemale}")
