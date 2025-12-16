4b460da7c657388007e8aadb646b341087b136ba
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2025-12-16 22:50:34 +0000

    fileRename for Find Variable type

---
+++ src/2_Find_Varibale_Type.py
@@ -0,0 +1,17 @@
+# Typecasting = the process of converting a variable from one data type to another
+# str()
+# int()
+# float()
+# bool()
+from random import sample
+
+name = "Ilankumaran Ilangovan"
+age = 35
+price = 4300.5
+isTrue = True
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(isTrue))
+
