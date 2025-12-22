9d640878ae0aec6b5b1113e64243fcf2ab44986a
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2025-12-22 21:38:13 +0000

    First Commit

--- src/4_User_input_And_Casting.py
+++ src/4_User_input_And_Casting.py
@@ -1,4 +1,13 @@
+# input("Enter: ") is used to take input from the user through the console/terminal.
 
+# "Enter: " is just a prompt message shown to the user.
+
+# It waits until the user types something and presses Enter.
+
+# It returns what the user typed as a string (str).
+
+x = input("Enter: ")
+print(type(x))   # <class 'str'>
 
 age = int(input("Enter age: "))
 salary = float(input("Enter salary: "))
