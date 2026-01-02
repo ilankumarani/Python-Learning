db783fcbf67d0b89a3cbfe89b8b511ddbad50adf
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2026-01-02 08:59:14 +0000

    First Commit

--- src/12_For_Loop_By_Range_Count.py
+++ src/12_For_Loop_By_Range_Count.py
@@ -7,7 +7,7 @@ for i in range(0, 10, 2):
 
 print(f"\n========= for Range Count down timer =========")
 ## for Range between 50(Inclusive) and 100(Exclusive) and Count
-for i in range(3, 0, -1):
+for i in range(5, 0, -1):
     print(f"{i}")
     tm.sleep(1)
 print(f"Happy new year", datetime.now().year, sep=" :: [", end="]")
