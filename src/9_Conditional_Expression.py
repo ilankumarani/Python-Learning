33128c112d3759bfef24ada9ad037acef14d60b5
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2026-01-03 02:14:00 +0000

    First Commit

--- src/9_Conditional_Expression.py
+++ src/9_Conditional_Expression.py
@@ -1,3 +1,9 @@
+# conditional expression = A one-line shortcut for the if-else statement (ternary operator)
+#                 Print or assign one of two values based on a condition
+#                 X if condition else Y
+
+
+
 # Python ternary operator— officially it’s called a conditional expression.
 
 # Syntax:
