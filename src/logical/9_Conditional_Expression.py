fa65e5f87fcad3c61328ff9f299f1bf06bda6398
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2026-01-08 14:35:20 +0000

    First Commit

--- src/logical/9_Conditional_Expression.py
+++ src/logical/9_Conditional_Expression.py
@@ -1,7 +1,7 @@
 # conditional expression = A one-line shortcut for the if-else statement (ternary operator)
 #                 Print or assign one of two values based on a condition
 #                 X if condition else Y
-
+import logging
 
 
 # Python ternary operator— officially it’s called a conditional expression.
@@ -14,11 +14,10 @@ def main()->None:
     if x.isdecimal():
         x = int(x)
         message = "Right to vote" if x >= 18 else "Can not vote"
-
+        logging.warning(f"{message}")
     else:
-        message = "Please enter the number"
-
-    print(f"{message}")
+        message = " *** Please enter the number ***!!!"
+        logging.error(f"{message}")
 
 
 if __name__ == "__main__":
