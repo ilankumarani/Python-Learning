13952aed02313175c852900179cb1eb9bbfbb2b6
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2026-01-10 01:05:09 +0000

    First Commit

--- src/0_Python-Version.py
+++ src/0_Python-Version.py
@@ -1,11 +1,13 @@
+import string
 import sys
-
+import random
 
 
 
 if __name__ == "__main__":
     print(f"Python version in number  :: {sys.winver}")
-
     print(f"Python version :: {sys.version}")
-
     print(f"Python version :: {sys.executable}")
+
+    print(f"Help :: {help(random)}")
+    print([n for n in dir(string) if not n.startswith("_")])
